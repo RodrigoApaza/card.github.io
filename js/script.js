f503e3c3c9c81194e51ec9ca641e0b7de50030b8
Anibal-Lenovo-Windows <lightdestroyers.arca@gmail.com>
2023-06-10 11:53:46 -0400

    Archivo inicial

---
+++ js/script.js
@@ -0,0 +1,56 @@
+let fechaActual = new Date();
+
+let dia = fechaActual.getDate();
+let mes = fechaActual.getMonth();
+let anio = fechaActual.getFullYear();
+
+const boton = document.getElementById('boton');
+const textoOculto = document.getElementById('texto-oculto');    
+let meses = [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre"
+];
+
+document.getElementById('fecha').innerText = dia+" de "+meses[mes]+" de "+anio;
+
+boton.addEventListener('click', function () {
+
+    if (!textoOculto.classList.contains("desoculto")) {
+        textoOculto.classList.add('desoculto');
+        textoOculto.style.display = "block";
+
+    }
+});
+
+
+window.addEventListener("load", function() {
+    var progress = document.getElementById("progreso");
+    var duration = 10000; // Duración de la animación en milisegundos
+    var startValue = 0; // Valor inicial
+    var endValue = 100; // Valor final
+    var interval = duration / (endValue - startValue); // Intervalo de actualización
+  
+    function animateProgress() {
+      var currentValue = startValue;
+      var timer = setInterval(function() {
+        currentValue++;
+        progress.value = currentValue;
+        if (currentValue >= endValue) {
+          clearInterval(timer);
+          animateProgress(); // Llamada recursiva para repetir la animación
+        }
+      }, interval);
+    }
+  
+    animateProgress(); // Iniciar la animación al cargar la página
+  });
